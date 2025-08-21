e50362dc69577c74696f377b284ec70200b63fad
priyanshu-kr <k2priyanshu@gmail.com>
2025-08-21 13:56:44 +0530

    modified list.py

--- list.py
+++ list.py
@@ -1,2 +1,18 @@
 list1 = [1, 2, "hello", "4"]
-print(list1[1:4])
+# print(list1[1:4])
+
+list1.append(4)
+# print(list1)
+
+list2 = list1.copy()
+# print(list2)
+
+list1.clear()
+# print(list1)
+
+# print(list2.count(4))
+# print(list2.count('4'))
+
+list1.insert(0, 'g')
+list1.insert(1, 'k')
+print(list1[0])
