f5b28d7b95e09505e86465b74bc55bf9a8ea527f
priyanshu-kr <k2priyanshu@gmail.com>
2025-08-27 01:07:47 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+input_str = input("Enter a string: ")
+
+if is_palindrome(input_str):
+    print(f'"{input_str}" is a palindrome.')
+else:
+    print(f'"{input_str}" is not a palindrome.')
+
