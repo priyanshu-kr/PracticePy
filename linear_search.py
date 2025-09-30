31b9511543bca6d320bcfc680575cc2127b90bda
priyanshu-kr <k2priyanshu@gmail.com>
2025-09-30 23:50:10 +0530

    fixed linear_search

--- linear_search.py
+++ linear_search.py
@@ -1,18 +1,22 @@
-# Simple linear search algo which finds only first occurrence of target
-def linear_search(arr, target):
-    for i, value in enumerate(arr):
-        if value == target:
-            return i
-    return -1
+## Simple linear search algo which finds only first occurrence of target
 
-arr = list(map(int, input("Enter numbers separated by space: ").split()))
-print(arr)
+# def linear_search(arr, target):
+#     for i, value in enumerate(arr):
+#         if value == target:
+#             return i
+#     return -1
 
-target = int(input("Enter element to search: "))
+# arr = list(map(int, input("Enter numbers separated by space: ").split()))
+# print(arr)
 
-print("Element is found at index " + str(linear_search(arr, target)) + ".")
+# target = int(input("Enter element to search: "))
+
+# print("Element is found at index " + str(linear_search(arr, target)) + ".")
+
+
 
 # linear search which finds all occurrences of target
+
 def linear_search_all(arr, target):
     indices = []
     for i, value in enumerate(arr):
