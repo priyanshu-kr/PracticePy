86fabcad45dc9a41e26732c780fbd0bccfde9360
priyanshu-kr <k2priyanshu@gmail.com>
2025-09-30 23:49:01 +0530

    modified linear_search

--- linear_search.py
+++ linear_search.py
@@ -1,3 +1,4 @@
+# Simple linear search algo which finds only first occurrence of target
 def linear_search(arr, target):
     for i, value in enumerate(arr):
         if value == target:
@@ -10,3 +11,24 @@ print(arr)
 target = int(input("Enter element to search: "))
 
 print("Element is found at index " + str(linear_search(arr, target)) + ".")
+
+# linear search which finds all occurrences of target
+def linear_search_all(arr, target):
+    indices = []
+    for i, value in enumerate(arr):
+        if value == target:
+            indices.append(i)
+    if len(indices) == 0:
+        return -1  # Return -1 if target not found at all
+    return indices
+
+arr = list(map(int, input("Enter numbers separated by space: ").split()))
+print(arr)
+
+target = int(input("Enter element to search: "))
+
+result = linear_search_all(arr, target)
+if result == -1:
+    print("Element not found.")
+else:
+    print("Element found at indices:", result)
