3c861e0d56b1d8dfc4e850f4b4aeebe5aa8ee20c
priyanshu-kr <k2priyanshu@gmail.com>
2025-10-04 22:27:04 +0530

    updated insertion_sort

--- sort_insertion.py
+++ sort_insertion.py
@@ -1,5 +1,5 @@
 def insertion_sort(arr):
-    for i in range(l, len(arr)):
+    for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
         while j >= 0 and arr[j] > key:
@@ -8,3 +8,4 @@ def insertion_sort(arr):
         arr[j + 1] = key
     return arr
 
+print(insertion_sort([3,41,2,8,54,9,24]))
